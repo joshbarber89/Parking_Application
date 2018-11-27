f93963417837acee26deb9719ca1c78f12a52e0f
joshbarber89 <josh_barber89@hotmail.com>
2018-11-27 16:26:24 -0500

    Imported google sdk's for using the map, so starting code

--- app/src/main/java/com/final_project/josh/parking_application/MainActivity.java
+++ app/src/main/java/com/final_project/josh/parking_application/MainActivity.java
@@ -1,13 +1,42 @@
 package com.final_project.josh.parking_application;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+    private void init(){
+        //https://www.youtube.com/watch?v=M0bYvXlhgSI&t=6s
+    }
+    public boolean isServicesOK(){
+        Log.d(TAG,"isServicesOK: checking google services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if(available == ConnectionResult.SUCCESS){
+            Log.d(TAG,"isServicesOK: checking google services version");
+            return true;
+        }else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,available,ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else{
+            Toast.makeText(this,"You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
